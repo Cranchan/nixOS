7fe90106e35aed0225d8f511d298a7795d5eb003
Dawn <klusignolo54@gmail.com>
2026-06-24 14:32:44 -0500

    edit flake.nix to call ./modules instead of all individually

--- flake.nix
+++ flake.nix
@@ -10,16 +10,7 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-		# Core modules
-			./modules/hardware-configuration.nix
-			./modules/configuration.nix
-		
-		# Packages list
-			./modules/packages.nix
-		
-		# Desktop and shell config
-			./modules/gnome.nix
-			./modules/zsh.nix
+			./modules
         ];
       };
     };
