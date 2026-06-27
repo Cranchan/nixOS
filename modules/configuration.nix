92a65a474c6099d6cff27fa9bc73fc9e2d2512b0
Dawn <klusignolo54@gmail.com>
2026-06-27 18:58:55 -0500

    proprietary codecs added to configuration.nix

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -62,7 +62,15 @@
 
     ];
   };
-
+	environment.systemPackages = with pkgs; [
+	  ffmpeg-full
+	  gst_all_1.gstreamer
+	  gst_all_1.gst-plugins-base
+	  gst_all_1.gst-plugins-good
+	  gst_all_1.gst-plugins-bad
+	  gst_all_1.gst-plugins-ugly
+	  gst_all_1.gst-libav
+];
   system.stateVersion = "26.05";
 
 }
