dbcdb08bced230c1fa3844cec910403535dcde1c
Dawn <klusignolo54@gmail.com>
2026-06-27 19:17:26 -0500

    changed to standard linux kernel, due to zen kernel build failure

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -6,7 +6,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
