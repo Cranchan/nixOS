33cf2657212fdc7b37f765b914560662bf05ecd7
Dawn <klusignolo54@gmail.com>
2026-06-28 10:46:32 -0500

    incorporated home manager theming section to gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,11 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the GNOME Desktop Environment.
+  # --- System-Level GNOME Core Settings ---
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   
-  # Disable gnome core applications so only needed applications will be added as defined below in system package
   services.gnome.core-apps.enable = false;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
@@ -16,19 +15,18 @@
     xterm
   ];
 
-
-environment.systemPackages = with pkgs; [
-  
+  environment.systemPackages = with pkgs; [
     # Gnome Software
     ptyxis
     nautilus
     rewaita
     gnome-tweaks
-    gedit    
+    gedit        
     
     # Icons
     papirus-icon-theme
-	adwaita-icon-theme
+    adwaita-icon-theme
+
     # Fonts
     nerd-fonts.adwaita-mono
     
@@ -38,4 +36,48 @@ environment.systemPackages = with pkgs; [
     gnomeExtensions.middle-click-to-close-in-overview
     gnomeExtensions.user-themes  
   ];
+
+  # --- User-Level Home Manager Settings ---
+  home-manager.users.cranchan = { ... }: {
+    home.stateVersion = "26.05";
+
+    dconf.settings = {
+      # 1. Background Wallpapers
+      "org/gnome/desktop/background" = {
+        picture-uri = "file://${../assets/catpuccin-nix.jpg}";
+        picture-uri-dark = "file://${../assets/catpuccin-nix.jpg}";
+        picture-options = "stretched";
+      };
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file://${../assets/catpuccin-nix.jpg}";
+      };
+
+      # 2. Extensions
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          pkgs.gnomeExtensions.user-themes.extensionUuid
+          pkgs.gnomeExtensions.dash-to-dock.extensionUuid
+          pkgs.gnomeExtensions.blur-my-shell.extensionUuid
+          pkgs.gnomeExtensions.middle-click-to-close-in-overview.extensionUuid
+        ];
+      };
+
+      # 3. Shell Theme
+      "org/gnome/shell/extensions/user-theme" = {
+        name = "Rewaita";
+      };
+    };
+
+    # 4. Application Theme (GTK & Icons)
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Rewaita"; 
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+      };
+    };
+  };
 }
